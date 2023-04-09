882369f60e2ee2866e02fe8819c0646e4cab4dc7
Huy Nguyen <huy.ducnguyen@zaitenllc.com>
2023-04-09 18:12:45 +0700

    change css

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             min = (Math.floor( Math.floor((today - yourDate) / 1000) / 60)) % 60,
             sec =  Math.floor((today - yourDate) / 1000) % 60,
             count = Math.floor((today - yourDate) / 1000 / 60 / 60 / 24);
-            counter.textContent = count - 1;
+            counter.textContent = count;
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
